a836d7aeb3af58b339f4cedac5bfa24092e3d505
olesya <olegha2000@gmail.com>
2021-11-19 17:47:11 +0600

    окружности рандомного цвета

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 import random
-from PyQt5 import uic
+from circles import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor
 
 
-class App(QMainWindow):
+class App(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
 
         self.qp = QPainter()
@@ -27,7 +27,8 @@ class App(QMainWindow):
         self.repaint()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor("yellow"))
+        col = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        qp.setBrush(QColor(*col))
         coords = [random.randint(50, 550), random.randint(50, 450)]
         size = random.randint(5, 50)
         qp.drawEllipse(*coords, size, size)
